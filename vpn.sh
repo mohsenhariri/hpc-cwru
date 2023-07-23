dae26e65bde2bde4568411e9091ecf965e722e59
Mohsen Hariri <mhariri68@gmail.com>
2023-07-23 18:44:11 -0400

    vpn client alias

--- vpn.sh
+++
@@ -1,2 +0,0 @@
-path./to/forticlientsslvpn_cli --server vpn2.case.edu:443 --vpnuser caseID 
-
---
+++ vpn_alias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "alias vpncase='path/to/forticlientsslvpn_cli --server vpn2.case.edu:443 --vpnuser caseID'" >> ~/.bashrc
+
+source ~/.bashrc
